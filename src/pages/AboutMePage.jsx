90d2c4eb77402a890c122bd496433b89cb380c5a
khabLexander <mantillagka@hotmail.com>
2022-11-25 19:03:58 -0500

    Latest

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -9,6 +9,12 @@ export const AboutMePage = () => {
         <h3>Kevin Alexander Mantilla G</h3>
         <span>
           Hi, I'm a Software Developer From Quito-Ecuador
+          currently I am beign partf of Kruger Star contest, I have some skills as
+          FrontEnd developer specially with React JS and Angular, also I have some skills as 
+          BackEnd developer with JAVA using spring boot, php using Laravel, python using Odoo and Django, and 
+          javascript using Node Js with Express.
+          
+          I always want to learn something new and educate myself.
         </span>
       </div>
     </div>
