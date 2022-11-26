21c9ffc55e7e192ad38d04a9bf5c7367e2c6d828
khabLexander <mantillagka@hotmail.com>
2022-11-25 21:11:53 -0500

    Fix text birthdate

--- src/pages/AgeCalcPage.jsx
+++ src/pages/AgeCalcPage.jsx
@@ -4,7 +4,7 @@ import '../styles/AgeCalPage.css'
 export const AgeCalcPage = () => {
   return (
     <div className="age-content animate__animated animate__fadeIn">
-      <h1>Enter your first and last name</h1>
+      <h1>Enter your BirthDate</h1>
       <Calendar />
     </div>
   )
